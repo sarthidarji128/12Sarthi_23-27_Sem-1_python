0442222ae067362f4256d0003eeb303f2595ce61
sar12.8thi <142773841+sarthidarji128@users.noreply.github.com>
2024-01-12 22:55:02 +0530

    2_4_CubeNumberInList.py

--- 2_4_CubeNumberInList.py
+++ 2_4_CubeNumberInList.py
@@ -1,5 +1,13 @@
+# Create a list of numbers
 c = [2, 3, 4, 5, 6, 7, 8, 9]
-cube = [(num**3) for num in c]
+
+# Create a new list with the cubes of the numbers using list comprehension
+cube = [(num**3) for num in c]  # Calculate the cube of each number in c and create a new list
+
+# Print the original list
+print("Original list:")
 print(c)
-print()
+
+# Print the list of cubes
+print("\nList of cubes:")
 print(cube)
