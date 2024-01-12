a98a017eaf4c1a8f3a3e87172cc6290e265d98c9
sar12.8thi <142773841+sarthidarji128@users.noreply.github.com>
2024-01-12 22:56:09 +0530

    2_5_Check_two_list.py

--- 2_5_Check_two_list.py
+++ 2_5_Check_two_list.py
@@ -1,6 +1,9 @@
+# Create two dictionaries with the same key-value pairs
 dict1 = {"name": "sarthi", "age": 17}
 dict2 = {"name": "sarthi", "age": 17}
 
-result = dict1 == dict2
+# Compare the dictionaries using the `==` operator
+result = dict1 == dict2  # Check if they have the same key-value pairs (order doesn't matter)
 
-print("Result :",result)
+# Print the result of the comparison
+print("Result:", result)
