90e5838a4bbf9b0092cdd8475f15e7fbacc729b1
sar12.8thi <142773841+sarthidarji128@users.noreply.github.com>
2024-01-12 22:58:30 +0530

    2_7_List_modification.py

--- 2_7_List_modification.py
+++ 2_7_List_modification.py
@@ -1,40 +1,50 @@
+# Create a list of numbers
 a = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
+# Print the complete list
 print("Complete list:", a)
 print()
-print("Fourth element:", a[3])
+
+# Accessing elements:
+print("Fourth element:", a[3])  # Accessing by index
 print()
-print("List from 0th to 4th index:", a[:4])
+print("List from 0th to 4th index:", a[:4])  # Slicing to get elements from index 0 to 3
 print()
-print("List from -7th to 3rd element:", a[-7:3])
+print("List from -7th to 3rd element:", a[-7:3])  # Slicing from index -7 to 2 (not including 3)
 print()
-a.append(110)
+
+# Modifying the list:
+a.append(110)  # Adding an element to the end
 print("List after appending 110:", a)
 print()
-a.sort()
+a.sort()  # Sorting the list in ascending order
 print("Sorted list:", a)
-
 print()
-element = a.pop()
+
+# Removing elements:
+element = a.pop()  # Removing and returning the last element
 print("Popped element:", element)
 print("List after popping:", a)
 print()
-
-a.remove(60)
+a.remove(60)  # Removing the first occurrence of 60
 print("List after removing 60:", a)
 print()
 
-a.insert(2, 45)
+# Inserting elements:
+a.insert(2, 45)  # Inserting 45 at index 2
 print("List after inserting 45 at index 2:", a)
-
 print()
-count = a.count(30)
+
+# Counting elements:
+count = a.count(30)  # Counting the occurrences of 30
 print("Occurrence of 30 in the list:", count)
-
 print()
-a.extend([120, 130])
+
+# Extending the list:
+a.extend([120, 130])  # Adding multiple elements at the end
 print("List after extending:", a)
-
 print()
-a.reverse()
+
+# Reversing the list:
+a.reverse()  # Reversing the order of elements
 print("List after reversing:", a)
