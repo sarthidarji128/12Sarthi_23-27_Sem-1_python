3a9cd20dfe4d05b80c2532d9da2a5f08fbda421f
sar12.8thi <142773841+sarthidarji128@users.noreply.github.com>
2024-01-12 22:52:44 +0530

    2_2_Reverse_method.py

--- 2_2_Reverse_method.py
+++ 2_2_Reverse_method.py
@@ -1,15 +1,15 @@
-#reverse method
+# Reverse a list using the `reverse` method
 my_list1 = [11, 22, 33, 44, 55]
-my_list1.reverse()
+my_list1.reverse()  # Reverse the elements of the list in-place
 
-print("reverse method:")
+print("Reverse using `reverse` method:")
 for item in my_list1:
-    print(item,end=" ")
-print()
+    print(item, end=" ")
+print()  # Print a newline
 
-#slice method
-my_list2 = [13, 32,53,74, 95]
+# Reverse a list using slicing
+my_list2 = [13, 32, 53, 74, 95]
 
-print("reverse using slicing:")
-for item in my_list2[::-1]:  
-    print(item,end=" ")
+print("Reverse using slicing:")
+for item in my_list2[::-1]:  # Iterate through the reversed list using slicing
+    print(item, end=" ")
