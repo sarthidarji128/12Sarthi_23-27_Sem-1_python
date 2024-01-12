6148f9f8ed3ec2a859e8089021ece936cd4f7825
sar12.8thi <142773841+sarthidarji128@users.noreply.github.com>
2024-01-12 22:43:32 +0530

    1_5_Find_vowel.py

--- 1_5_Find_vowel.py
+++ 1_5_Find_vowel.py
@@ -1,6 +1,11 @@
+# Define a string containing all vowels (both lowercase and uppercase)
 vowels = "aeiouAEIOU"
+
+# Prompt the user to enter a character
 character = input("Enter a character: ")
-if character in vowels:
-    print(character,"is a vowel.")
-else:
-    print(character," is not a vowel.")
+
+# Check if the entered character is a vowel using the "in" operator
+if character in vowels:  # True if the character is found within the vowels string
+  print(character, "is a vowel.")  # Print a message indicating the character is a vowel
+else:  # False if the character is not found in the vowels string
+  print(character, "is not a vowel.")  # Print a message indicating the character is not a vowel
