41c4fa343f878405f9cd7f6006f5af49e78c270e
sar12.8thi <142773841+sarthidarji128@users.noreply.github.com>
2024-01-12 22:41:48 +0530

    1_3_Even or odd.py

--- 1_3_Even or odd.py
+++ 1_3_Even or odd.py
@@ -1,6 +1,8 @@
-number = int(input("Enter a number: "))
+# Prompt the user to enter a number
+number = int(input("Enter a number: "))  # Convert the input to an integer
 
-if number % 2 == 0:
-    print(number," is even.")
-else:
-    print(number,"is odd.")
+# Check if the number is even or odd using the modulo operator
+if number % 2 == 0:  # True if the remainder after dividing by 2 is 0 (even)
+  print(number, "is even.")  # Print a message indicating the number is even
+else:  # False if the remainder is not 0 (odd)
+  print(number, "is odd.")  # Print a message indicating the number is odd
