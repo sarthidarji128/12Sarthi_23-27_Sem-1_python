0b9edcef367a5be223152293188e6a2e1e5bd9a7
sar12.8thi <142773841+sarthidarji128@users.noreply.github.com>
2024-01-12 23:15:21 +0530

    5_1_import_package.py

--- 5_1_import_package.py
+++ 5_1_import_package.py
@@ -1,8 +1,11 @@
-import mymod
+import mymod  # Import the module named 'mymod'
 
+# Get user input for numbers
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
+# Call the add_numbers function from the imported module
 result = mymod.add_numbers(num1, num2)
 
+# Print the calculated sum
 print("The sum of", num1, "and", num2, "is", result)
