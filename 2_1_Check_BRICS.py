d84d3b260df14f09c6e8a4932a89fafcb12cd2fd
sar12.8thi <142773841+sarthidarji128@users.noreply.github.com>
2024-01-12 22:51:37 +0530

    2_1_Check_BRICS.py

--- 2_1_Check_BRICS.py
+++ 2_1_Check_BRICS.py
@@ -1,6 +1,11 @@
+# Define a list of BRICS countries
 brics_countries = ["Brazil", "Russia", "India", "China", "South Africa"]
+
+# Get a country name from the user
 country_name = str(input("Enter a country name: "))
-if country_name in brics_countries:
-    print(country_name, "is a member of BRICS.")
+
+# Check if the entered country is in the BRICS list
+if country_name in brics_countries:  # Use the "in" operator to check membership
+  print(country_name, "is a member of BRICS.")
 else:
-    print(country_name, "is not a member of BRICS.")
+  print(country_name, "is not a member of BRICS.")
