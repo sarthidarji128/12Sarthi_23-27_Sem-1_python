77187b7a3356bd2f1815f163ee4cf1ec4c0ad30e
sar12.8thi <142773841+sarthidarji128@users.noreply.github.com>
2024-01-12 22:49:03 +0530

    1_8_1_Pattern_1.py

--- 1_8_1_Pattern_1.py
+++ 1_8_1_Pattern_1.py
@@ -1,3 +1,6 @@
-n=int(input("Enter the number of rows :"))
-for i in range(1, n):
-     print("*" * i)
+# Get the number of rows from the user
+n = int(input("Enter the number of rows: "))
+
+# Use a loop to print the pyramid pattern
+for i in range(1, n):  # Iterate from 1 to n-1 (inclusive)
+  print("*" * i)  # Print i asterisks in each row
