425660d9edd50bba130c014059e739866ff8ab1c
sar12.8thi <142773841+sarthidarji128@users.noreply.github.com>
2024-01-12 22:49:55 +0530

    1_8_2_Pattern_2.py

--- 1_8_2_Pattern_2.py
+++ 1_8_2_Pattern_2.py
@@ -1,3 +1,6 @@
-n=int(input("Enter the value of rows :"))
-for i in range(1, n+1):
-      print(str(i) * i+" " * (n+1 - i))
+# Get the number of rows from the user
+n = int(input("Enter the value of rows: "))
+
+# Use a loop to print the right-angled triangle pattern
+for i in range(1, n + 1):  # Iterate from 1 to n (inclusive)
+  print(str(i) * i + " " * (n + 1 - i))  # Print i copies of the current number followed by spaces
